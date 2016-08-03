5b3ccdc55e729af75ff4acb5d2a375ef8ada318d
Samina Chhatwal <csaminak@gmail.com>
2016-08-03 17:35:01 -0400

    updated addItem function to be able to reset form field once item has been added

--- src/js/inventory.controller.js
+++ src/js/inventory.controller.js
@@ -17,8 +17,20 @@
         this.totalPrice = totalPrice;
         this.inventory = handleInventory.findAll();
         this.newItem = {};
-        this.addItem = handleInventory.addItem;
+        this.addItem = addItem;
 
+
+        /**
+         * Takes the item submitted and saves it into newItem after it has
+         * passed into the service function. Then sets newItem to empty to
+         * reset field.
+         * which does other things to it.
+         * @param {Object}  item   item to add into inventory with properties
+         */
+        function addItem(item) {
+            that.newItem = handleInventory.addItem(item);
+            that.newItem = {};
+        }
 
         /**
          * Accepts a sorting type string and sets the sortType to that value
