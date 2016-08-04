529ca89dbac783e166cf70dc3cf0f4c83b634cd7
Samina Chhatwal <csaminak@gmail.com>
2016-08-03 23:23:26 -0400

    added login function for service

--- build/js/main.js
+++ build/js/main.js
@@ -159,12 +159,20 @@
     angular.module('shopular')
         .factory('userMgmt', UserMgmtService);
 
-
+    /**
+     * return an object that contains data and functions to handle the service
+     * @return      {Object}    contains data relevant to the service
+     */
     function UserMgmtService() {
         return {
-
+            login: login
         };
     }
+
+    function login() {
+        
+    }
+
 
 
 
